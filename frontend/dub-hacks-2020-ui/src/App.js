97191e8ccaf5ee043b179a50b4c4c1880b914bb4
Stone Tao <stonezt2019@gmail.com>
2020-10-17 13:49:30 -0700

    less

--- frontend/dub-hacks-2020-ui/src/App.js
+++ frontend/dub-hacks-2020-ui/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import "./App.css";
-import "antd/dist/antd.css";
+import "./styles/index.less";
 import { Button, Space } from "antd";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
