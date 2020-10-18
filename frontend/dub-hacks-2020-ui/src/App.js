18ac6ae7a4ad4aded2f36b0446e3b49b2877484c
leestanley <stanleylee91776@gmail.com>
2020-10-17 21:03:31 -0700

    meow

--- frontend/dub-hacks-2020-ui/src/App.js
+++ frontend/dub-hacks-2020-ui/src/App.js
@@ -8,12 +8,9 @@ import "./styles/reset.less";
 import { Route, Switch} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import HomePage from './containers/HomePage';
-<<<<<<< HEAD
 import BuyPage from './containers/BuyPage';
-=======
 import LoginPage from './containers/LoginPage';
 import RegisterPage from './containers/RegisterPage';
->>>>>>> 6b36a7750abed9ffe6c52644a4b858f04729c50d
 
 const store = configureStore();
 
