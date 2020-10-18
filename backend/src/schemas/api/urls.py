e56a19b2fcd9632387420565614ec95bc335efbd
Stone Tao <stonezt2019@gmail.com>
2020-10-17 18:19:22 -0700

    recommendations

--- backend/src/schemas/api/urls.py
+++ backend/src/schemas/api/urls.py
@@ -4,13 +4,15 @@ from django.urls import path
 from .views import (
   AllUsersView,
   CreateUserView,
-  GetUserView
+  GetUserView,
+  GetRecommendationsView
 )
 
 urlpatterns = [
     path('', AllUsersView.as_view()),
     path('create/',CreateUserView.as_view()),
     path('get_user/<email>', GetUserView.as_view()),
+    path('user/<email>/recommendations', GetRecommendationsView.as_view()),
     path('items/', AllItemsView.as_view()),
     path('items_create/', CreateItemView.as_view()),
     path('items/<id>', GetItemView.as_view()),
