084d0773d22b5a2364371de7e0eec1c83e9cf9c9
leestanley <stanleylee91776@gmail.com>
2020-10-18 01:41:40 -0700

    MEOWWW

--- frontend/dub-hacks-2020-ui/src/components/Navbar/index.js
+++ frontend/dub-hacks-2020-ui/src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import 'antd/dist/antd.css';
 import './style.less';
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.svg';
 import profilepic from '../../assets/stanley-pic.png';
 import { Input } from 'antd';
 import {NavLink} from 'react-router-dom';
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className="search-bar">
         <Search placeholder="Search for your favorite items" onSearch={value => console.log(value)} enterButton />
         </div>
-        <div className="profile"><Avatar icon={<UserOutlined />} size={42} icon src={profilepic} /></div>
+        <div className="profile"><img src={profilepic} alt=""/></div>
     </nav>
 
   )
--- frontend/dub-hacks-2020-ui/src/containers/LoginForm.jsx
+++ frontend/dub-hacks-2020-ui/src/containers/LoginForm.jsx
@@ -1,16 +1,12 @@
 import React, { useContext } from 'react';
 import LoginForm from '../components/LoginForm';
 import UserContext from '../UserContext';
-import Navbar from '../components/Navbar';
 
 
 const LoginFormContainer = () => {
   const {user, setUser} = useContext(UserContext);
   return (
-    <>
-    <Navbar />
     <LoginForm setUser={setUser}/>
-    </>
   );
 };
 
